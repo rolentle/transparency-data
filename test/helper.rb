856d76200db0ecf538a2cefa093480733236a972
Rolen Le <rolentle@gmail.com>
2015-03-13 20:32:26 -0400

    Clean up test helper file

--- test/helper.rb
+++ test/helper.rb
@@ -1,13 +1,11 @@
-require 'test/unit'
+gem 'minitest'
+require 'minitest/autorun'
+require 'yaml'
 require 'pathname'
 
 require 'shoulda'
-require 'matchy'
 require 'mocha'
-require 'fakeweb'
 require 'vcr'
-
-require 'redgreen'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
@@ -16,8 +14,6 @@ require 'transparency_data'
 api_config = YAML.load_file(File.dirname(__FILE__) + '/../api.yml')
 TransparencyData.api_key = api_config['key']
 TransparencyData.api_domain = api_config['domain'] if api_config['domain']
-
-#FakeWeb.allow_net_connect = false
 
 VCR.configure do |c|
   c.cassette_library_dir = 'test/fixtures/vcr_cassettes'
